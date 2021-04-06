735a2f23b64951c13a9566daf934e14380d2269d
tartie sorel <sorelaugustin@gmail.com>
2021-04-06 21:20:16 +0100

    setting up then window

--- MarchingSquares/MainWindow.xaml.cs
+++ MarchingSquares/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MarchingSquares
 {
@@ -24,5 +12,13 @@ namespace MarchingSquares
         {
             InitializeComponent();
         }
+
+        #region Key Down Event Handler
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                Application.Current.Shutdown();
+        }
+        #endregion
     }
 }
