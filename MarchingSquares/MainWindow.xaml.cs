67d495a3f1289634612791f1091864c18d2ca5a3
tartie sorel <sorelaugustin@gmail.com>
2021-04-06 21:26:42 +0100

    content render added

--- MarchingSquares/MainWindow.xaml.cs
+++ MarchingSquares/MainWindow.xaml.cs
@@ -8,6 +8,11 @@ namespace MarchingSquares
     /// </summary>
     public partial class MainWindow : Window
     {
+        private float[,] field;
+        private int rez = 10;
+        private int cols;
+        private int rows;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,5 +25,12 @@ namespace MarchingSquares
                 Application.Current.Shutdown();
         }
         #endregion
+
+        private void Window_ContentRendered(object sender, System.EventArgs e)
+        {
+            cols = (int)canvas.ActualWidth / rez;
+            rows = (int)canvas.ActualHeight / rez;
+            field = new float[cols, rows];
+        }
     }
 }
