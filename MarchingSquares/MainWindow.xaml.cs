88b261ec01a08d6546e9c25516f100b22487c818
tartie sorel <sorelaugustin@gmail.com>
2021-04-10 21:01:18 +0100

    timer removed

--- MarchingSquares/MainWindow.xaml.cs
+++ MarchingSquares/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Threading;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,8 +26,7 @@ namespace MarchingSquares
         //private Line[,] lines;
         private readonly float increment = 0.1f;
         private float zOff = 0;
-
-        private Timer aTimer;
+        
         private readonly BackgroundWorker worker = new BackgroundWorker();
 
         public MainWindow()
@@ -78,118 +78,118 @@ namespace MarchingSquares
 
 
             }));
-
-            aTimer = new Timer();
-            aTimer.Elapsed += new ElapsedEventHandler(HandleDraw);
-            aTimer.Interval = 100;
-            aTimer.Enabled = true;
-            aTimer.Start();
+            HandleDraw();
         }
 
-        private void HandleDraw(object sender, EventArgs e)
+        private void HandleDraw()
         {
-            float xOff = 0;
-            for (int i = 0; i < cols; i++)
+            while (true)
             {
-                xOff += increment;
-                float yOff = 0;
-                for (int j = 0; j < rows; j++)
+                float xOff = 0;
+                for (int i = 0; i < cols; i++)
                 {
-                    field[i, j] = (float)noise.Evaluate(xOff, yOff, zOff);
-                    yOff += increment;
-                }
-            }
-            zOff += 0.03f;
-            //field[i, j] = (float)random.NextDouble(); 
-
-            //for (int i = 0; i < cols; i++)
-            //    for (int j = 0; j < rows; j++)
-            //    {
-            //        if (field[i, j] < 0)
-            //          ellipses[i, j].Fill = Brushes.White;
-            //        else 
-            //            ellipses[i, j].Fill = Brushes.Black;
-
-
-            //        ellipses[i, j].Opacity = Math.Abs(field[i, j]);
-            //        Canvas.SetLeft(ellipses[i, j], i * rez - ellipses[i, j].ActualWidth / 2); // add half of the ellipse;
-            //        Canvas.SetTop(ellipses[i, j], j * rez - ellipses[i, j].ActualHeight / 2);
-            //    }
-
-            List<Line> listOfLinesToRemove = new List<Line>();
-            
-            Application.Current.Dispatcher.Invoke(new Action(() =>
-            {
-                foreach (var item in canvas.Children)
-                    if (item.GetType() == typeof(Line))
-                        listOfLinesToRemove.Add(item as Line);
-
-                foreach (Line item in listOfLinesToRemove)
-                    canvas.Children.Remove(item);
-            
-
-            for (int i = 0; i < cols - 1; i++)
-                for (int j = 0; j < rows - 1; j++)
-                {
-                    float x = i * rez;
-                    float y = j * rez;
-                    Vector a = new Vector(x + rez * 0.5, y);
-                    Vector b = new Vector(x + rez, y + rez * 0.5);
-                    Vector c = new Vector(x + rez * 0.5, y + rez);
-                    Vector d = new Vector(x, y + rez * 0.5);
-
-                    int state = GetState(Math.Ceiling(field[i, j]), Math.Ceiling(field[i + 1, j]), Math.Ceiling(field[i + 1, j + 1]), Math.Ceiling(field[i, j + 1]));
-
-                    switch (state)
+                    xOff += increment;
+                    float yOff = 0;
+                    for (int j = 0; j < rows; j++)
                     {
-                        case 1:
-                            DrawLine(c, d);
-                            break;
-                        case 2:
-                            DrawLine(b, c);
-                            break;
-                        case 3:
-                            DrawLine(b, d);
-                            break;
-                        case 4:
-                            DrawLine(a, b);
-                            break;
-                        case 5:
-                            DrawLine(a, d);
-                            DrawLine(b, c);
-                            break;
-                        case 6:
-                            DrawLine(a, c);
-                            break;
-                        case 7:
-                            DrawLine(a, d);
-                            break;
-                        case 8:
-                            DrawLine(a, d);
-                            break;
-                        case 9:
-                            DrawLine(a, c);
-                            break;
-                        case 10:
-                            DrawLine(a, b);
-                            DrawLine(c, d);
-                            break;
-                        case 11:
-                            DrawLine(a, b);
-                            break;
-                        case 12:
-                            DrawLine(b, d);
-                            break;
-                        case 13:
-                            DrawLine(b, c);
-                            break;
-                        case 14:
-                            DrawLine(c, d);
-                            break;
+                        field[i, j] = (float)noise.Evaluate(xOff, yOff, zOff);
+                        yOff += increment;
                     }
                 }
+                zOff += 0.03f;
+                //field[i, j] = (float)random.NextDouble(); 
 
-            }));
+                //for (int i = 0; i < cols; i++)
+                //    for (int j = 0; j < rows; j++)
+                //    {
+                //        if (field[i, j] < 0)
+                //          ellipses[i, j].Fill = Brushes.White;
+                //        else 
+                //            ellipses[i, j].Fill = Brushes.Black;
+
+
+                //        ellipses[i, j].Opacity = Math.Abs(field[i, j]);
+                //        Canvas.SetLeft(ellipses[i, j], i * rez - ellipses[i, j].ActualWidth / 2); // add half of the ellipse;
+                //        Canvas.SetTop(ellipses[i, j], j * rez - ellipses[i, j].ActualHeight / 2);
+                //    }
+
+                List<Line> listOfLinesToRemove = new List<Line>();
+
+                Application.Current.Dispatcher.Invoke(new Action(() =>
+                {
+                    foreach (var item in canvas.Children)
+                        if (item.GetType() == typeof(Line))
+                            listOfLinesToRemove.Add(item as Line);
+
+                    foreach (Line item in listOfLinesToRemove)
+                        canvas.Children.Remove(item);
+
+
+                    for (int i = 0; i < cols - 1; i++)
+                        for (int j = 0; j < rows - 1; j++)
+                        {
+                            float x = i * rez;
+                            float y = j * rez;
+                            Vector a = new Vector(x + rez * 0.5, y);
+                            Vector b = new Vector(x + rez, y + rez * 0.5);
+                            Vector c = new Vector(x + rez * 0.5, y + rez);
+                            Vector d = new Vector(x, y + rez * 0.5);
+
+                            int state = GetState(Math.Ceiling(field[i, j]), Math.Ceiling(field[i + 1, j]), Math.Ceiling(field[i + 1, j + 1]), Math.Ceiling(field[i, j + 1]));
+
+                            switch (state)
+                            {
+                                case 1:
+                                    DrawLine(c, d);
+                                    break;
+                                case 2:
+                                    DrawLine(b, c);
+                                    break;
+                                case 3:
+                                    DrawLine(b, d);
+                                    break;
+                                case 4:
+                                    DrawLine(a, b);
+                                    break;
+                                case 5:
+                                    DrawLine(a, d);
+                                    DrawLine(b, c);
+                                    break;
+                                case 6:
+                                    DrawLine(a, c);
+                                    break;
+                                case 7:
+                                    DrawLine(a, d);
+                                    break;
+                                case 8:
+                                    DrawLine(a, d);
+                                    break;
+                                case 9:
+                                    DrawLine(a, c);
+                                    break;
+                                case 10:
+                                    DrawLine(a, b);
+                                    DrawLine(c, d);
+                                    break;
+                                case 11:
+                                    DrawLine(a, b);
+                                    break;
+                                case 12:
+                                    DrawLine(b, d);
+                                    break;
+                                case 13:
+                                    DrawLine(b, c);
+                                    break;
+                                case 14:
+                                    DrawLine(c, d);
+                                    break;
+                            }
+                        }
+
+                }));
+
+                Thread.Sleep(100);
+            }
         }
 
         private void DrawLine(Vector v1, Vector v2)
